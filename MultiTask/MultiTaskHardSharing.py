e9e71f3fa50dd9d560da71f17c06a6c0f3902a20
KilianCallebaut <kilian.callebaut@gmail.com>
2021-03-22 21:47:01 +0100

    Update MultiTaskHardSharing.py

--- MultiTask/MultiTaskHardSharing.py
+++ MultiTask/MultiTaskHardSharing.py
@@ -2,11 +2,6 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
-from MultiTask.FFNN import FFNN
-from MultiTask.TaskDependentLayers import TaskDependentLayers
-from MultiTask.TaskDependentSoftmax import TaskDependentSoftmax
-from pytorch_lightning.metrics.functional.classification import accuracy
-
 
 # https://gist.github.com/AvivNavon/cf2071ffaadfb11dded915f7f4bd638e
 # https://medium.com/@adrian.waelchli/3-simple-tricks-that-will-change-the-way-you-debug-pytorch-5c940aa68b03
