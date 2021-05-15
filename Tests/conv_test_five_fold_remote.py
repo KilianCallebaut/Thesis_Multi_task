9d3327162ac67e8e5dc7d7598ca8b3b95186f1f5
KilianCallebaut <kilian.callebaut@gmail.com>
2021-05-15 19:21:05 +0200

    Update conv_test_five_fold_remote.py

--- Tests/conv_test_five_fold_remote.py
+++ Tests/conv_test_five_fold_remote.py
@@ -19,7 +19,6 @@ from Training.Results import Results
 from Training.Training import Training
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-drive = 'F'
 
 
 def run_datasets(dataset_list, extraction_params):
@@ -28,31 +27,31 @@ def run_datasets(dataset_list, extraction_params):
 
     if 0 in dataset_list:
         asvspoof = ASVspoof2015(**extraction_params,
-                                object_path=r'\data\Thesis_Multi_task\data\Data_Readers\ASVspoof2015_{}')
+                                object_path=r'/data/Thesis_Multi_task/data/Data_Readers/ASVspoof2015_{}')
         taskDatasets.append(asvspoof.taskDataset)
     if 1 in dataset_list:
         chenaudio = ChenAudiosetDataset(**extraction_params,
-                                        object_path=r'\data\Thesis_Multi_task\data\Data_Readers\ChenAudiosetDataset')
+                                        object_path=r'/data/Thesis_Multi_task/data/Data_Readers/ChenAudiosetDataset')
         taskDatasets.append(chenaudio.taskDataset)
     if 2 in dataset_list:
         dcaseScene = DCASE2017_SS(**extraction_params,
-                                  object_path=r'\data\Thesis_Multi_task\data\Data_Readers\DCASE2017_SS_{}')
+                                  object_path=r'/data/Thesis_Multi_task/data/Data_Readers/DCASE2017_SS_{}')
         taskDatasets.append(dcaseScene.taskDataset)
     if 3 in dataset_list:
         fsdkaggle = FSDKaggle2018(**extraction_params,
-                                  object_path=r'\data\Thesis_Multi_task\data\Data_Readers\FSDKaggle2018')
+                                  object_path=r'/data/Thesis_Multi_task/data/Data_Readers/FSDKaggle2018')
         taskDatasets.append(fsdkaggle.taskDataset)
     if 4 in dataset_list:
         ravdess = Ravdess(**extraction_params,
-                          object_path=r'\data\Thesis_Multi_task\data\Data_Readers\Ravdess')
+                          object_path=r'/data/Thesis_Multi_task/data/Data_Readers/Ravdess')
         taskDatasets.append(ravdess.taskDataset)
     if 5 in dataset_list:
         speechcommands = SpeechCommands(**extraction_params,
-                                        object_path=r'\data\Thesis_Multi_task\data\Data_Readers\SpeechCommands_{}')
+                                        object_path=r'/data/Thesis_Multi_task/data/Data_Readers/SpeechCommands_{}')
         taskDatasets.append(speechcommands.taskDataset)
     if 6 in dataset_list:
         dcaseEvents = DCASE2017_SE(**extraction_params,
-                                   object_path=r'\data\Thesis_Multi_task\data\Data_Readers\DCASE2017_SE_{}')
+                                   object_path=r'/data/Thesis_Multi_task/data/Data_Readers/DCASE2017_SE_{}')
         taskDatasets.append(dcaseEvents.taskDataset)
     print('loaded all datasets')
 
@@ -73,7 +72,7 @@ def run_test(eval_dataset, meta_params, results):
 
 
 def run_five_fold(dataset_list):
-    model_checkpoints_path = drive + r":\Thesis_Results\Model_Checkpoints"
+    model_checkpoints_path = "/data/Thesis_Results/Model_Checkpoints"
     meta_params = read_config('meta_params_cnn_MelSpectrogram')
     extraction_params = read_config('extraction_params_cnn_MelSpectrogram')
     # extraction_params = read_config('extraction_params_cnn_mfcc')
